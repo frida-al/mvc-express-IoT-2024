b552b89c47457d995c23139bc3980e665fea236a
Frida Arcadia <frida.arcadialv@gmail.com>
2024-10-17 17:50:45 -0600

    [ADD] Add CRUD

--- src/controllers/student.ts
+++ src/controllers/student.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
-import { findAll } from "../services/student";
+import { deleteById, findAll, insert, update } from "../services/student";
+import { Student } from "../interfaces/student";
 
 // Obtener todos los alumnos
 export const getStudents = async (req: Request, res: Response) => {
@@ -9,5 +10,33 @@ export const getStudents = async (req: Request, res: Response) => {
     res.status(200).json(students);
   } catch (error) {
     res.status(400).json({ message: "Error al obtener alumnos", error });
+  }
+};
+export const createStudent = async (req: Request, res: Response) => {
+  try {
+    const student: Student = req.body;
+    await insert(student);
+    res.status(201).json({ message: "Alumno creado exitosamente" });
+  } catch (error) {
+    res.status(400).json({ message: "Error al crear alumno", error });
+  }
+};
+export const updateStudent = async (req: Request, res: Response) => {
+  try {
+    const id = Number.parseInt(req.params.id);
+    const student: Student = req.body;
+    await update(id, student);
+    res.status(201).json({ message: "Alumno actualizado exitosamente" });
+  } catch (error) {
+    res.status(400).json({ message: "Error al actualizar el alumno", error });
+  }
+};
+export const deleteStudent = async (req: Request, res: Response) => {
+  try {
+    const id = Number.parseInt(req.params.id);
+    await deleteById(id);
+    res.status(201).json({ message: "Alumno eliminado exitosamente" });
+  } catch (error) {
+    res.status(400).json({ message: "Error al eliminar el alumno", error });
   }
 };
--- src/services/student.ts
+++ src/services/student.ts
@@ -1,6 +1,21 @@
-import { findAllStudents } from "../models/student";
+import { Student } from "../interfaces/student";
+import {
+  findAllStudents,
+  insertStudent,
+  updateStudent,
+  deleteStudent,
+} from "../models/student";
 
 // Obtener todos los alumnos
 export const findAll = async () => {
   return await findAllStudents();
+};
+export const insert = async (student: Student) => {
+  return await insertStudent(student);
+};
+export const update = async (id: number, student: Student) => {
+  return await updateStudent(id, student);
+};
+export const deleteById = async (id: number) => {
+  return await deleteStudent(id);
 };
