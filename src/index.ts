b99ff4739a4deeb3556140a35829e3bf64dbdd77
Frida Arcadia <frida.arcadialv@gmail.com>
2024-10-31 15:20:40 -0600

    [ADD] README

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import express from "express";
 import studentRouter from "./routes/student";
 import unknownResource from "./middlewares/unknown-resource";
+import unknownError from "./middlewares/unknown-error";
 
 //Para poder acceder a las variables del ambiente (.env)
 config();
@@ -15,6 +16,7 @@ app.use("/student", studentRouter);
 
 //Middlewares
 app.use(unknownResource);
+app.use(unknownError);
 
 app.listen(process.env.SERVER_PORT, function () {
   console.log("Escuchando puerto " + process.env.SERVER_PORT);
