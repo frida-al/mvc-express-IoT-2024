eac545f91ae1467d7b3a83b16e08850218e47af9
Frida Arcadia <frida.arcadialv@gmail.com>
2024-10-31 22:10:37 -0600

    [ADD] validations and errors

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
-import { config } from "dotenv";
+import DotenvFlow from "dotenv-flow";
 import express from "express";
 import studentRouter from "./routes/student";
 import unknownResource from "./middlewares/unknown-resource";
 import unknownError from "./middlewares/unknown-error";
+import testRoutes from "./routes/test";
+import validationError from "./middlewares/validation-error";
 
 //Para poder acceder a las variables del ambiente (.env)
-config();
+if (process.env.NODE_ENV !== "production") {
+  DotenvFlow.config();
+}
 
 const app = express();
 
 // Middleware para parsear JSON
 app.use(express.json());
 
-app.use("/student", studentRouter);
+app.use("/api/v1/student", studentRouter);
+
+app.use("/error", testRoutes);
 
 //Middlewares
+app.use(validationError);
 app.use(unknownResource);
 app.use(unknownError);
 
